0ea9534c4f33fa1c326a0ad39258968ad44b1d1a
Ankushkr930 <royankush963@gmail.com>
2025-04-23 01:14:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
 
 
   </StrictMode>,
