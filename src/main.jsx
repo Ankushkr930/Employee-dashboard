2cefa8119adc1c96c35a3b2a7e7bdd2256b1b405
Ankushkr930 <royankush963@gmail.com>
2025-05-12 23:17:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import AuthProvider from './context/AuthProvider.jsx'
 
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
 
+    <AuthProvider>
+      <App />
+    </AuthProvider>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
-
-
-  </StrictMode>,
+  </React.StrictMode>,
 )
